9c8b35754151ed55ac1957a0aaa4504480a4a418
jayachandrands <jaidurai1422@gmail.com>
2022-06-30 22:30:43 -0700

    task18

---
+++ function-sub.js
@@ -0,0 +1,9 @@
+let a = 25;
+let b = 15;
+function subtraction(x,y){
+    //let x = 25, y = 15;
+    subtraction = x-y;
+    return subtraction;
+}
+let result  = subtraction(a,b);
+console.log(result);
